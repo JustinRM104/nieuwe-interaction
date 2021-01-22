b6bad68b3f3058a2f9fdc1ba8a1586deed4a69a7
JustinRM104 <justrom104@gmail.com>
2021-01-22 12:31:35 +0100

    update css style en javascript toevoeging

--- interactionobserver.js
+++ interactionobserver.js
@@ -1,9 +1,23 @@
 let links = document.querySelectorAll('nav a');
+let secties = document.querySelectorAll('section');
+
+const opties = {
+    treshold: 1
+};
 
 const verwijderActief = () => {
     links.forEach((link) => {
         if( link.classList = 'actief') {
             link.classList.remove('actief');
+        }
+    });
+}
+
+const verwerkDoorsnijding = (entries, observer) => {
+    entries.forEach( entry => {
+        if ( entry.isIntersecting ) {
+           let link = zoekBijpassendeLink('#' + entry.target.id);
+           maakActief(link);
         }
     });
 }
@@ -20,3 +34,9 @@ links.forEach((link) => {
         window.location = e.target.href;
     })
 });
+
+
+const zoekBijpassendeLink = (id) => {
+    let link = document.querySelector('nav a[href="' + id + '"]');
+    return link;
+}
